53b47b0df08ffd1b91c19f5f2933e88887afa993
David Lovas <dlovas@rexovas.com>
2020-04-28 16:56:00 -0400

    update cft.service.yaml with www route43 record and update buildspec & deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 # export VERSION=$(cat package.json | jq -r '.version')
 STAGE='production'
 
-# aws cloudformation deploy --stack-name demo-backend-config-${STAGE} --parameter-overrides stage=${STAGE} --template-file ./templates/cloudformation/cft.service.yaml --capabilities CAPABILITY_NAMED_IAM
+# aws cloudformation deploy --stack-name demo-backend-config-${STAGE} --parameter-overrides Stage=${STAGE} --template-file ./templates/cloudformation/cft.service.yaml --capabilities CAPABILITY_NAMED_IAM
 aws cloudformation deploy --stack-name demo-backend-cicd --template-file ./templates/cloudformation/cft.cicd.yaml --capabilities CAPABILITY_NAMED_IAM
 # aws --region us-west-2 cloudformation deploy --stack-name demo-backend-cicd --template-file ../templates/cloudformation/cft.cicd.yaml # --capabilities CAPABILITY_NAMED_IAM
 # aws --region us-west-2 cloudformation deploy --stack-name demo-backend-config-development --template-file ../templates/cloudformation/cft.service.yaml # --capabilities CAPABILITY_NAMED_IAM
