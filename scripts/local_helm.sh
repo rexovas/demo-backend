33273db0214b61063a0194203a978b7c047fadb3
David Lovas <dlovas@rexovas.com>
2020-04-29 01:07:59 -0400

    working build

--- scripts/local_helm.sh
+++ scripts/local_helm.sh
@@ -8,4 +8,4 @@ TAG=$STAGE-$VERSION
 echo $TAG
 
 kubectl apply -f ./templates/helm/$NAME/namespace-$STAGE.yaml
-helm upgrade --install $NAME --namespace $NAME-$STAGE -f ./templates/helm/$NAME/values-$STAGE.yaml ./templates/helm/$NAME --set image.tag=$TAG  --set ingress.host=$HOST --set ingress.domain=$DOMAIN --debug
+helm upgrade --install $NAME --namespace $NAME-$STAGE -f ./templates/helm/$NAME/values-$STAGE.yaml ./templates/helm/$NAME --set image.tag=$TAG  --set ingress.host=$HOST --set ingress.domain=$DOMAIN --set deployment.port=$PORT --debug
