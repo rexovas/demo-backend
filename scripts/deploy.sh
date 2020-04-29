44cf6f8bb4a7dca2e134e45b231753da8b36afd9
David Lovas <dlovas@rexovas.com>
2020-04-29 15:45:29 -0400

    create environments.sh script, remove environment variables from cicd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-# export VERSION=$(cat package.json | jq -r '.version')
-STAGE='production'
-NAME='demo-backend'
-PORT=5000
-HOST='demo'
+source ./environment.sh
 
 aws cloudformation deploy \
 --stack-name demo-backend-cicd \
 --template-file ./templates/cloudformation/cft.cicd.yaml \
---parameter-overrides Stage=$STAGE Name=$NAME Port=$PORT Host=$HOST \
+--parameter-overrides Stage=$STAGE Name=$NAME \
 --capabilities CAPABILITY_NAMED_IAM
