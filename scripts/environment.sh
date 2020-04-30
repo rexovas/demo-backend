2191c57fbe18b14604c9e6277478527f2852e61c
David Lovas <dlovas@rexovas.com>
2020-04-30 03:38:53 -0400

    finalize certificate issuance script

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 export NAME="demo-backend"
-# - export VERSION=$(node -e "console.log(require('./package.json').version)")
 export VERSION="1.0.0"
 if [ -z "$VERSION" ]; then echo "VERSION variable is missing"; exit 1; fi
+export ELB="a169916f7200c4926a3f515a782505d7"
+export HOSTED_ZONE="Z050060699L3CKSWFLIF"
+export TOP_DOMAIN=".rex.vision"
 export HOST="demo"
 export DOMAIN=".api.rex.vision"
 export PORT=5000
