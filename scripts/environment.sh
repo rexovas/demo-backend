0d301bd58fdc169b70a8161ff46c2568942aff0b
David Lovas <dlovas@rexovas.com>
2020-05-03 17:15:56 -0400

    modify environment.sh script to source version from src/version.py file

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 export NAME="demo-backend"
-export VERSION="1.0.0"
+VERSION="$(cat src/version.py | awk -F= '/^__version__/ {print $2}')"
+export VERSION="$(echo $VERSION | tr -d "'")"
 if [ -z "$VERSION" ]; then echo "VERSION variable is missing"; exit 1; fi
 export ELB="a169916f7200c4926a3f515a782505d7"
 export HOSTED_ZONE="Z050060699L3CKSWFLIF"
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 redis-server &
 sleep 5
-# export FLASK_APP=src/app.py
+# COMMENT LINE BELOW IF RUNNING DEVELOPMENT SERVER
 gunicorn -w 4 -b 0.0.0.0 src:app --log-level debug
+# UNCOMMENT LINES BELOW TO RUN DEVELOPMENT SERVER
+# export FLASK_APP=src/app.py
 # flask run -h 0.0.0.0
